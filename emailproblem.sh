2a20d0515e55afa39d945dddb72cc844220ca6eb
Swathi-Shetty-gif <swathishetty9924@gmail.com>
2020-10-14 12:31:45 +0530

    lets close all options

--- emailproblem.sh
+++ emailproblem.sh
@@ -3,7 +3,7 @@ echo "validating email adress witha regex"
 
 read -p "enter your email" email
 
-pat='^[a-zA-Z0-9]+([.+-_][a-zA-Z0-9]+)*@[a-zA-Z0-9]+[.][a-z]{2,4}$'
+pat='^[a-zA-Z0-9]+([.+-_][a-zA-Z0-9]+)*@[a-zA-Z0-9]+[.][a-z]{2,4}([.][a-z]{2}*$'
 
 if [ $email =~ $pat ]
 then
