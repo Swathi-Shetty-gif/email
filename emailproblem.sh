8d426ee660afa858d0a9028e7b3aa41bf298c6be
Swathi-Shetty-gif <swathishetty9924@gmail.com>
2020-10-14 11:24:22 +0530

    start with abc...

--- emailproblem.sh
+++ emailproblem.sh
@@ -1,2 +1,11 @@
 #!/bin/bash
 echo "validating email adress witha regex"
+read -p "enter your email" email
+pat='^[a-zA-Z0-9]+$'
+
+if [ $email =~ $pat ]
+then
+	echo "valid"
+else
+	echo "not valid"
+fi 
